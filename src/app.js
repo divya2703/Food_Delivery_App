30477a3f282d29faafcea2fe217ac7a148eb5d25
Divya Kumari <divya27@Divyas-MacBook-Air.local>
2021-09-30 18:21:20 +0530

    setup: server running on port

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+require('./db/db')
+const port = process.env.PORT || 9000;
+const app = express();
+
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,5 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1:27017/oren?retryWrites=true&w=majority', {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
     useUnifiedTopology: true
 });
